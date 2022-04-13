9f07ea73bf280f09e344b259119e156da2b6e33d
artemkriachko <kriachkoartem@gmail.com>
2022-04-13 22:31:14 +0300

    update v1.1

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,78 @@
 
 const canvas = document.getElementById('canvas');
-const startButton = document.querySelector('.start');
-const stopButton = document.querySelector('.stop');
-const score = document.getElementById('score');
+const canvasParameters = {
+  width: window.innerWidth,
+  height: window.innerHeight*0.7,
+  backgroundColor: 'lightblue',
+}
+canvas.width = canvasParameters.width;
+canvas.height = canvasParameters.height;
+let gameParameters = {
+  isGameStarted: false,
+  numberBullets: 10,
+  lifes: 3,
+}
+const startButton = document.querySelector('.start'),
+    stopButton = document.querySelector('.stop'),
+    score = document.getElementById('score'),
+    lifes = document.querySelector('#lifes'),
+    numberBullets = document.querySelector('#bullets');
+
+lifes.innerHTML = gameParameters.lifes;
+numberBullets.innerHTML = gameParameters.numberBullets;
+
+
+
+let styles = `<style>
+h2.game_name{
+text-transform: uppercase;
+text-align: center;
+}
+.container{
+display: flex;
+flex-direction: column;
+align-items: center;
+}
+.control_buttons button{
+background: none;
+border: none;
+font-size: 22px;
+font-weight: 600;
+cursor: pointer;
+transition: 0.3s;
+}
+.control_buttons button:hover{
+transform: scale(1.2);
+}
+.start{
+color: green;
+}
+.stop{
+color: red;
+}
+.game_statistic{
+margin: 30px 0;
+width: 100%;
+display: flex;
+justify-content: space-evenly;
+font-size: 20px;
+}
+.game_statistic > div span{
+font-weight: 600;
+}
+
+</style>`
+document.querySelector('head').insertAdjacentHTML('beforeend',styles)
 
 const ctx = canvas.getContext('2d');
 
 
-const canvasParameters = {
-  width:canvas.clientWidth,
-  height:canvas.clientHeight,
-  backgroundColor: 'lightblue',
-}
+
+
+
 const plane = {
   positionX: -30,
-  positionY: 50,
+  positionY: 20,
   width: 30,
   heigh: 3,
   hit: false,
@@ -34,7 +92,6 @@ const gunParameters = {
   },
 }
 
-let isGameStarted = false;
 
 function drawGun (){
   ctx.fillRect(gunParameters.positionX,gunParameters.positionY,gunParameters.width,gunParameters.height)
@@ -48,86 +105,132 @@ const start = {
 }
 
 
-
-
 canvas.style.backgroundColor = canvasParameters.backgroundColor;
 ctx.fillStyle = gunParameters.color;
 ctx.fillRect(gunParameters.positionX,gunParameters.positionY,gunParameters.width,gunParameters.height)
 
-let NewPlane = function  (){
-  const newPlane = {...plane}
-  newPlane.speed = +(parseInt((Math.random()*10))/5)+1
-  function movePlane(){
-    ctx.clearRect(0,0,canvasParameters.width,canvasParameters.height)
-    newPlane.positionX += newPlane.speed
-    ctx.fillRect(newPlane.positionX,newPlane.positionY,newPlane.width,newPlane.heigh)
-    if (isGameStarted === false){
-      ctx.clearRect(0,0,canvasParameters.width,canvasParameters.height)
-    } else if ((newPlane.positionX>canvasParameters.width) ||
-        (newPlane.hit === true)) {
-      NewPlane()
-    } else {
-      requestAnimationFrame(movePlane)
-    }
-    pl = newPlane
-  }
-  movePlane()
-}
-function shoot (event){
-  let newBullet = {...start};
-  const bulletStep = {
-    x: (event.offsetX-newBullet.x)/60,
-    y: (event.offsetY-newBullet.y)/60,
-  }
 
-  function drawBullet (){
-    newBullet.x = newBullet.x + bulletStep.x
-    newBullet.y = newBullet.y + bulletStep.y
-    if ((newBullet.x > pl.positionX) && (newBullet.x < pl.positionX+pl.width) &&
-        (newBullet.y < pl.positionY + pl.heigh) && (newBullet.y > pl.positionY-5)){
-      pl.hit = true;
-      +score.innerText++;
 
-    }
-
-    if (isGameStarted === true){
-      if ((newBullet.x > 0 && newBullet.x < canvasParameters.width)&&(newBullet.y > 0 && newBullet.y < canvasParameters.height)){
-        ctx.fillRect(newBullet.x,newBullet.y,gunParameters.bullet.width,gunParameters.bullet.height)
-      }}
-    requestAnimationFrame(drawBullet)
-    return newBullet;
-  }
-  drawBullet()
-}
-function showResult (){
-  if (+score.innerText === 0 ){
-    alert(`HAHAHAHA! LOOSER!!`)
-  } else if ((score.innerText > 0) && (score.innerText <= 5)){
-    alert(`Hmm.. Not bad!`)
-  } else if (score.innerText > 5){
-    alert(`Very GOOD!`)
-  }
-}
 
 
 function game (){
+  gameParameters.isGameStarted = true;
+  let newGameParameters = {...gameParameters}
   score.innerText = 0;
-  if (isGameStarted === false){
+  lifes.innerHTML = newGameParameters.lifes;
+  numberBullets.innerHTML = newGameParameters.numberBullets
+
+  if (newGameParameters.isGameStarted === true){
     ctx.clearRect(0,0,canvasParameters.width,canvasParameters.height);
     ctx.fillRect(gunParameters.positionX,gunParameters.positionY,gunParameters.width,gunParameters.height)
-    isGameStarted = true;
+
+    let NewPlane = function  (){
+      const newPlane = {...plane}
+      newPlane.speed = +(parseInt((Math.random()*10))/5)+1;
+      newPlane.positionY = newPlane.positionY+(parseInt((Math.random()*100)))
+      pl = newPlane
+      function movePlane(){
+        ctx.clearRect(0,0,canvasParameters.width,canvasParameters.height)
+        newPlane.positionX += newPlane.speed
+        ctx.fillRect(pl.positionX,pl.positionY,pl.width,pl.heigh)
+        if (newGameParameters.isGameStarted === false){
+          ctx.clearRect(0,0,canvasParameters.width,canvasParameters.height)
+        } else if ((pl.positionX>canvasParameters.width)) {
+          if (newGameParameters.numberBullets === 0){
+            gameOver()
+          }
+          if (newGameParameters.lifes>0){
+            NewPlane()
+            newGameParameters.lifes--;
+            if (newGameParameters.lifes <= gameParameters.lifes/3){
+              lifes.style.color = 'red'
+            } else if (newGameParameters.lifes <= gameParameters.lifes/3*2){
+              lifes.style.color = 'orange'
+            }
+            lifes.innerHTML = newGameParameters.lifes
+          } else gameOver()
+        } else if (pl.hit === true){
+          NewPlane()
+        } else {
+          requestAnimationFrame(movePlane)
+        }
+
+      }
+      movePlane()
+    }
     NewPlane()
     drawGun()
 
 
 
+      function shoot (event){
+      if ((newGameParameters.numberBullets>0)&&
+          newGameParameters.isGameStarted === true){
 
+        newGameParameters.numberBullets--
+        if (newGameParameters.numberBullets <= gameParameters.numberBullets/3){
+          numberBullets.style.color = 'red'
+        } else if (newGameParameters.numberBullets <= gameParameters.numberBullets/3*2){
+          numberBullets.style.color = 'orange'
+        }
+        numberBullets.innerHTML = newGameParameters.numberBullets
+
+        let newBullet = {...start};
+        const bulletStep = {
+          x: (event.offsetX-newBullet.x)/60,
+          y: (event.offsetY-newBullet.y)/60,
+        }
+
+        function drawBullet (){
+          newBullet.x = newBullet.x + bulletStep.x
+          newBullet.y = newBullet.y + bulletStep.y
+          if ((newBullet.x > pl.positionX) && (newBullet.x < pl.positionX+pl.width) &&
+              (newBullet.y < pl.positionY + pl.heigh) && (newBullet.y > pl.positionY-10)){
+            pl.hit = true;
+            +score.innerText++;
+          }
+          if (newGameParameters.isGameStarted === true){
+            if ((newBullet.x > 0 && newBullet.x < canvasParameters.width)&&(newBullet.y > 0 && newBullet.y < canvasParameters.height)){
+              ctx.fillRect(newBullet.x,newBullet.y,gunParameters.bullet.width,gunParameters.bullet.height)
+            }}
+          requestAnimationFrame(drawBullet)
+          return newBullet;
+        }
+        drawBullet()
+      }
+    }
     canvas.addEventListener('click',shoot )
+
+
+
   }
+  function showResult (){
+   if (score.innerText > 20){
+      alert(`Very GOOD!`)
+    } else if (score.innerText > 15) {
+     alert(`Hmm.. Not bad!`)
+   } else if (score.innerText > 0) {
+     alert(`So-so...`)
+   } else if (+score.innerText === 0 ){
+      alert(`HAHAHAHA! LOOSER!!`)
+    }
+  }
+  function gameOver() {
+    gameParameters.isGameStarted = false
+    newGameParameters.isGameStarted = false;
+    showResult()
+  }
+  stopButton.addEventListener('click',function (){
+    if (newGameParameters.isGameStarted === true){
+      showResult()
+      gameParameters.isGameStarted = false;
+      newGameParameters.isGameStarted = false
+    }
+  })
   }
 
-  startButton.addEventListener('click',game);
-  stopButton.addEventListener('click',function (){
-    showResult()
-    isGameStarted = false;
-})
+  startButton.addEventListener('click',function (){
+    if (gameParameters.isGameStarted === false){
+      game()
+    }
+  });
